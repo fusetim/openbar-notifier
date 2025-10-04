2f1b94bcf06bd33355bca2fba6044eedb75014f0
FuseTim <fusetim.log@gmx.com>
2025-10-04 10:05:18 +0200

    refactor: move the "logic" inside OpenBarClient
    Also the bar is currently down... so let's just wait for now!

--- openbar-notifier/src/main.rs
+++ openbar-notifier/src/main.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use log::{info, error, debug};
 use openbar_notifier::openbar::{webconfig::{get_config_with_client}, OpenBarClient};
-use openbar_api::{apis::{auth_api::AuthApi, categories_api::CategoriesApi, items_api::ItemsApi}, models::{ConnectCardRequest, ItemState}};
+use openbar_api::models::{ItemState};
 
 #[tokio::main]
 async fn main() {
@@ -19,21 +19,7 @@ async fn main() {
     let pin = &args[2];
 
     // Create a Reqwest client with TLS Keylog enabled
-    let root_store = rustls::RootCertStore::from_iter(
-        webpki_roots::TLS_SERVER_ROOTS
-            .iter()
-            .cloned(),
-    );
-    let mut tls_client = rustls::ClientConfig::builder_with_provider(Arc::new(rustls::crypto::aws_lc_rs::default_provider()))
-        .with_safe_default_protocol_versions().expect("Failed to set protocol versions")
-        .with_root_certificates(root_store)
-        .with_no_client_auth();
-    tls_client.key_log = std::sync::Arc::new(rustls::KeyLogFile::new());
-    let http = reqwest::ClientBuilder::new()
-        .use_preconfigured_tls(tls_client)
-        .cookie_store(true)
-        .build()
-        .expect("Failed to create Reqwest client");
+    let http = create_http_client();
 
     // Get the Instance webconfig
     let webconfig = match get_config_with_client(&http, "https://bar.telecomnancy.net").await {
@@ -49,12 +35,10 @@ async fn main() {
     // Connect to OpenBar API
     let mut client = OpenBarClient::with_client(&webconfig.api, http);
     client.set_local_token(&webconfig.local_token);
-    let auth_api = client.as_auth();
 
     // Login
-    let auth_req = ConnectCardRequest::new(card_id.clone(), pin.clone());
-    match auth_api.connect_card(Some(auth_req)).await {
-        Ok(resp) => info!("Logged in successfully"),
+    match client.login_by_card(card_id, pin).await {
+        Ok(_resp) => info!("Logged in successfully"),
         Err(e) => {
             error!("Error during login: {:?}", e);
             return;
@@ -62,18 +46,16 @@ async fn main() {
     }
 
     // Get all products
-    let categories_api = client.as_categories();
-    match categories_api.get_categories(None).await {
+    match client.get_categories().await {
         Ok(categories) => {
             info!("Got {} categories:", categories.len());
             // - For each category, get items
-            let items_api = client.as_items();
             for category in categories {
                 let category_id = category.id;
-                match items_api.get_category_items(&category_id.to_string(), Some(0), Some(100), None).await {
+                match client.get_category_items(&category_id.to_string()).await {
                     Ok(items) => {
                         info!("Items in category {}:", category.name);
-                        for item in items.items {
+                        for item in items {
                             let status : &str;
                             if item.state == ItemState::ItemNotBuyable {
                                 status = "⛔";
@@ -93,8 +75,27 @@ async fn main() {
     }
 
     // Logout
-    match auth_api.logout().await {
+    match client.logout().await {
         Ok(_) => info!("Logged out successfully"),
         Err(e) => error!("Error during logout: {}", e),
     }
+}
+
+/// Create a Reqwest HTTP client with TLS Keylog support (easier to debug).
+fn create_http_client() -> reqwest::Client {
+    let root_store = rustls::RootCertStore::from_iter(
+        webpki_roots::TLS_SERVER_ROOTS
+            .iter()
+            .cloned(),
+    );
+    let mut tls_client = rustls::ClientConfig::builder_with_provider(Arc::new(rustls::crypto::aws_lc_rs::default_provider()))
+        .with_safe_default_protocol_versions().expect("Failed to set protocol versions")
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+    tls_client.key_log = std::sync::Arc::new(rustls::KeyLogFile::new());
+    reqwest::ClientBuilder::new()
+        .use_preconfigured_tls(tls_client)
+        .cookie_store(true)
+        .build()
+        .expect("Failed to create Reqwest client")
 }
--- openbar-notifier/src/openbar/client.rs
+++ openbar-notifier/src/openbar/client.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
 use openbar_api::apis::configuration::Configuration as BarConfiguration;
-use openbar_api::apis::auth_api::{AuthApiClient};
-use openbar_api::apis::categories_api::{CategoriesApiClient};
-use openbar_api::apis::items_api::{ItemsApiClient};
+use openbar_api::apis::auth_api::{AuthApi, AuthApiClient, ConnectCardError, LogoutError};
+use openbar_api::apis::categories_api::{CategoriesApi, CategoriesApiClient, GetCategoriesError};
+use openbar_api::apis::items_api::{GetCategoryItemsError, ItemsApi, ItemsApiClient};
+use openbar_api::models::{Account, Category, ConnectCardRequest, Item};
+use openbar_api::apis::Error as ApiError;
 
 /// `OpenBarClient` provides a convenient wrapper for interacting with the OpenBar API.
 /// It manages API configuration, authentication tokens, and exposes API clients.
@@ -42,6 +44,17 @@ impl OpenBarClient {
         }
     }
 
+    /// Creates a new OpenBarClient with a custom BarConfiguration.
+    /// 
+    /// This method is mostly useful for testing or advanced use cases where you need to
+    /// customize the entire configuration.
+    pub fn with_configuration(configuration: BarConfiguration) -> Self {
+        OpenBarClient {
+            bar_config: Arc::new(configuration),
+            ..Default::default()
+        }
+    }
+
     /// Set the local bearer access token for authentication.
     /// This token will be included in the `X-Local-Token` header for some API requests.
     pub fn set_local_token(&mut self, token: &str) {
@@ -54,17 +67,65 @@ impl OpenBarClient {
     }
 
     /// Get an instance of the AuthApiClient using the current configuration.
-    pub fn as_auth(&self) -> AuthApiClient {
+    fn as_auth(&self) -> AuthApiClient {
         AuthApiClient::new(self.bar_config.clone())
     }
 
     /// Get an instance of the CategoriesApiClient using the current configuration.
-    pub fn as_categories(&self) -> CategoriesApiClient {
+    fn as_categories(&self) -> CategoriesApiClient {
         CategoriesApiClient::new(self.bar_config.clone())
     }
 
     /// Get an instance of the ItemsApiClient using the current configuration.
-    pub fn as_items(&self) -> ItemsApiClient {
+    fn as_items(&self) -> ItemsApiClient {
         ItemsApiClient::new(self.bar_config.clone())
+    }
+
+    /// Log in using a card ID and PIN, returning the associated Account if successful.
+    /// This is a convenience method that wraps the `connect_card` method of the AuthApiClient.
+    /// 
+    /// Note: this method will modify the internal state of the client by setting the necessary auth token/cookies.
+    pub async fn login_by_card(&self, card_id: &str, pin: &str) -> Result<Option<Account>, ApiError<ConnectCardError>> {
+        let auth_api = self.as_auth();
+        let auth_req = ConnectCardRequest::new(card_id.to_owned(), pin.to_owned());
+        match auth_api.connect_card(Some(auth_req)).await {
+            Ok(resp) => {
+                if let Some(account) = resp.account {
+                    Ok(Some(*account))
+                } else {
+                    Ok(None)
+                }
+            },
+            Err(e) => Err(e),
+        }
+    }
+
+    /// Log out the current user by calling the `logout` method of the AuthApiClient.
+    /// 
+    /// Note: this method will modify the internal state of the client by clearing the auth token/cookies.
+    pub async fn logout(&self) -> Result<(), ApiError<LogoutError>> {
+        let auth_api = self.as_auth();
+        match auth_api.logout().await {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e),
+        }
+    }
+
+    /// Get all categories available in the OpenBar instance.
+    pub async fn get_categories(&self) -> Result<Vec<Category>, ApiError<GetCategoriesError>> {
+        let categories_api = self.as_categories();
+        match categories_api.get_categories(None).await {
+            Ok(categories) => Ok(categories),
+            Err(e) => Err(e),
+        }
+    }
+
+    /// Get items for a specific category by its ID.
+    pub async fn get_category_items(&self, category_id: &str) -> Result<Vec<Item>, ApiError<GetCategoryItemsError>> {
+        let items_api = self.as_items();
+        match items_api.get_category_items(category_id, Some(0), Some(100), None).await {
+            Ok(items) => Ok(items.items),
+            Err(e) => Err(e),
+        }
     }
 }
